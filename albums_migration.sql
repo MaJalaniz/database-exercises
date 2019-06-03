6ddbeaa4eb0e74b0d8fba9d0f35befced54e5b04
Matthew Alaniz <alaniz.matthew07@gmail.com>
2019-06-03 13:25:57 -0500

    created albums list in MySQL

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE IF NOT EXISTS albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) NOT NULL,
+    record_name VARCHAR(100) NOT NULL,
+    sales DECIMAL(10, 2) NOT NULL,
+    genre VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
